b52b1a5f85438ee043bcd3ecb6e82e4c1aa9f9fd
danil <danilskor2000@mail.ru>
2021-02-27 11:28:54 +0700

    Создан класс PhoneNumber

---
+++ ContactsApp/PhoneNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp
+{
+    public class PhoneNumber
+    {
+        private string _numberPhone;
+
+        public string NumberPhone
+        {
+            get { return _numberPhone;}
+            set
+            {
+                foreach (var var in value)
+                {
+                    if (!char.IsDigit(var))
+                    {
+                        throw new ArgumentException("Строка должна содержать только цифры");
+                    }
+                }
+
+                if (value.Length != 11)
+                {
+                    throw new ArgumentException("Номер должен состоять из 11 цифр");
+                }
+
+                if (value[0] != 7)
+                {
+                    throw new ArgumentException("Код страны должен быть 7");
+                }
+
+                _numberPhone = value;
+            }
+        }
+
+        public PhoneNumber(string numberPhone)
+        {
+            NumberPhone = numberPhone;
+        }
+    }
+}

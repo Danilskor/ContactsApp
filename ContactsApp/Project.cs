a476f15ea9f22d093029f9dfb6cb4a1dafcc0d06
danil <danilskor2000@mail.ru>
2021-02-27 16:43:56 +0700

    Добавлен класс Project

---
+++ ContactsApp/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp
+{
+    class Project
+    {
+        public List<Contact> Contacts { get; set; } = new List<Contact>();
+
+        public void AddElement (Contact newContact)
+        {
+            Contacts.Add(newContact);
+        }
+    }
+}
